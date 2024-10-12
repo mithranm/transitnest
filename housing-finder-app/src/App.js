784237454cdfa3babcbf808402a8ace3e10b5863
Om Roy <om.k.roy@gmail.com>
2024-10-12 13:39:05 -0400

    added some imports to the app

--- housing-finder-app/src/App.js
+++ housing-finder-app/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import HousingFinderApp from './components/HousingFinderApp';
+import { Loader } from '@googlemaps/js-api-loader';
 
 function App() {
   return (
@@ -8,5 +9,14 @@ function App() {
     </div>
   );
 }
+
+const apiOptions = {
+  apiKey: "AIzaSyAwXN7rYnU8FT8yZh4D94qYVFydrh151Vk"
+}
+
+const loader = new Loader(apiOptions);
+ loader.load().then(() => {
+   console.log('Maps JS API loaded');
+ });
 
 export default App;
