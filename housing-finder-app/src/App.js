3f9ee017e5e8624d16dc918876fde8b5dcf3613b
Om Roy <om.k.roy@gmail.com>
2024-10-12 13:40:57 -0400

    removed api key from frontend

--- housing-finder-app/src/App.js
+++ housing-finder-app/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import HousingFinderApp from './components/HousingFinderApp';
-import { Loader } from '@googlemaps/js-api-loader';
 
 function App() {
   return (
@@ -9,14 +8,4 @@ function App() {
     </div>
   );
 }
-
-const apiOptions = {
-  apiKey: "AIzaSyAwXN7rYnU8FT8yZh4D94qYVFydrh151Vk"
-}
-
-const loader = new Loader(apiOptions);
- loader.load().then(() => {
-   console.log('Maps JS API loaded');
- });
-
 export default App;
