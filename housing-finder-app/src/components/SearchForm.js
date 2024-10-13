c28941519fbf24b750c9122dd23d6716620f1016
itohti <itohti@gmail.com>
2024-10-12 23:10:39 -0400

    css changes

--- housing-finder-app/src/components/SearchForm.js
+++ housing-finder-app/src/components/SearchForm.js
@@ -18,11 +18,11 @@ const SearchForm = ({ onSearch }) => {
 
   return (
     <form onSubmit={handleSubmit} className="searchForm mb-4 grid grid-cols-2 gap-2">
-      <label>Budget</label>
+      <label className="labels">Budget</label>
       <input type="number" name="budget" value={formData.budget} onChange={handleChange} placeholder="Budget" className="p-2 border rounded" />
-      <label>Preferred distance</label>
+      <label className="labels">Preferred distance</label>
       <input type="number" name="maxDistance" value={formData.maxDistance} onChange={handleChange} placeholder="Max Distance (mi)" className="p-2 border rounded" />
-      <label>Work zipcode</label>
+      <label className="labels">Work zipcode</label>
       <input type="text" name="workZip" value={formData.workZip} onChange={handleChange} placeholder="Work Zip" className="p-2 border rounded" />
       <button type="submit" className="submission_button rounded">
         Search
