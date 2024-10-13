68e69627e03591ca28ba9f7bfad8c44a764d4541
Om Roy <om.k.roy@gmail.com>
2024-10-12 22:33:01 -0400

    fixed the NAVBAR

--- housing-finder-app/src/components/NavigationBar.js
+++ housing-finder-app/src/components/NavigationBar.js
@@ -3,7 +3,6 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const NavigationBar = () => {
   return (
